9a4c1a7126d1cdd4936a30df92cde17475e1786f
Akbar Indra Saputra <akbar.indra.saputra@students.untidar.ac.id>
2024-11-25 07:12:22 +0700

    Add files via upload

---
+++ GUI 2/InvalidSubnetException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI2;
+
+
+/**
+ *
+ * @author AKBAR
+ */
+public class InvalidSubnetException extends Exception {
+    public InvalidSubnetException(String message) {
+        super(message);
+    }
+}
